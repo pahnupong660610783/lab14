ff6ae224928d4e54d88ab590b082b3800cbf7f0c
pahnupong660610783 <141812519+pahnupong660610783@users.noreply.github.com>
2024-01-12 14:53:34 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+for(int i=1;i<N;i++){
+        T a=d[i];
+        int j;
+        for(j=i-1;j>=0;j--){
+        T b=d[j];
+        if(a>b){
+            d[j+1]=d[j];
+            }else{
+            break;
+            }
+    }
+    d[j+1]=a;
+    cout <<"Pass "<<i<<":";
+    for(int i = 0; i < 10; i++) cout << d[i] << " ";
+    cout<<endl;
+ }
 
 }
 
